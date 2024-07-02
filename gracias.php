8e9dc381da1de4950d31f34037245b82a8302a2b
Alejandro Saturno <alesaturno64@gmail.com>
2024-07-02 17:43:08 -0300

    creacion general del registro

---
+++ gracias.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro Completo</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <h1>¡Gracias por Registrarte!</h1>
+    </header>
+    <section id="thank-you-message">
+        <p>Tu registro ha sido completado con éxito. Hemos enviado una confirmación a tu correo electrónico.</p>
+        <p>Esperamos verte en el evento de Atacados Anónimos Tour 2024.</p>
+    </section>
+    <footer>
+        <p>&copy; 2024 Fortinet, Inc. Todos los derechos reservados.</p>
+    </footer>
+</body>
+</html>
